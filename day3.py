4000c14dd55f39004098a1054830cb7c43215bb6
Kevin Sivic <kevin.sivic@gmail.com>
2020-12-03 12:54:44 -0500

    Day 3 Part 2

--- day3.py
+++ day3.py
@@ -33,16 +33,31 @@ def read_input(file_name):
 
 
 def calculate_day3(hill):
+    coll_a = calc_collisions(hill, 1, 1)
+    coll_b = calc_collisions(hill, 3, 1)
+    coll_c = calc_collisions(hill, 5, 1)
+    coll_d = calc_collisions(hill, 7, 1)
+    coll_e = calc_collisions(hill, 1, 2)
+
+    collisions = coll_a * coll_b * coll_c * coll_d * coll_e
+
+    return collisions
+
+
+def calc_collisions(hill, x_change, y_change):
     collisions = 0
     x = 0
-    max_x = len(hill[0]) - 1 # 0 indexed and remove newline at the end.
-    for y in range(0, len(hill)):
+    y = 0
+    max_x = len(hill[0]) - 1  # 0 indexed and remove newline at the end.
+    while y < len(hill):
         current_loc = hill[y][x]
         if current_loc == '#':
             collisions += 1
-        x += 3
-        if (x >= max_x):
+        x += x_change
+        if x >= max_x:
             x = x - max_x
+
+        y = y + y_change
 
     return collisions
 
