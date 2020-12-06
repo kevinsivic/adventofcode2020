4cebc8db47811403eb3dfde254002867432ab81d
Kevin Sivic <kevin.sivic@gmail.com>
2020-12-06 18:49:17 -0500

    Day 4 Part 2

--- day4.py
+++ day4.py
@@ -13,6 +13,37 @@ class Passport(object):
             if key not in self.fields:
                 return False
 
+        if not (1920 <= int(self.fields["byr"]) <= 2002):
+            return False
+
+        if not (2010 <= int(self.fields["iyr"]) <= 2020):
+            return False
+
+        if not (2020 <= int(self.fields["eyr"]) <= 2030):
+            return False
+
+        p = re.compile("^(\d+)(cm|in)$")
+        m = p.match(self.fields["hgt"])
+        if m is None:
+            return False
+        if m.group(2) == "cm":
+            if not (150 <= int(m.group(1)) <= 193):
+                return False
+        elif m.group(2) == "in":
+            if not (59 <= int(m.group(1)) <= 76):
+                return False
+
+        p = re.compile("^#[A-Fa-f0-9]{6}$")
+        if p.match(self.fields["hcl"]) is None:
+            return False
+
+        if self.fields["ecl"] not in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]:
+            return False
+
+        p = re.compile("^\d{9}$")
+        if p.match(self.fields["pid"]) is None:
+            return False
+
         return True
 
 
