98ff4cf8cccba22c2531d7c169ad9e1afa7253ac
Kevin Sivic <kevin.sivic@gmail.com>
2020-12-06 20:52:55 -0500

    Day 6 Part 2

--- day6.py
+++ day6.py
@@ -2,19 +2,33 @@ def read_input(file_name):
     file = open(file_name, 'r')
     groups = list()
     group = dict()
+    num_members = 0
 
     line: str
     for line in file.readlines():
         if line == "\n":
+            filter_unanimous(group, num_members)
             groups.append(group)
             group = dict()
+            num_members = 0
         else:
+            num_members = num_members + 1
             for answer in line.strip():
-                group[answer] = 1
+                count = 0
+                if answer in group:
+                    count = group[answer]
+                group[answer] = count + 1
 
+    filter_unanimous(group, num_members)
     groups.append(group)
 
     return groups
+
+
+def filter_unanimous(group, num_members):
+    for question, count in group.copy().items():
+        if count < num_members:
+            group.pop(question)
 
 
 def sum_groups(groups):
