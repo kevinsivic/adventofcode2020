ef251660ce718f5c737689b819296aff64fa2ac6
Kevin Sivic <kevin.sivic@gmail.com>
2020-12-06 19:45:39 -0500

    Day 5 Part 2

--- day5.py
+++ day5.py
@@ -25,10 +25,7 @@ def find_max_seat(seats):
     max_seat = -1
 
     for seat in seats:
-        column = find_pos(seat['column_definition'])
-        row = find_pos(seat['row_definition'])
-
-        seat_id = row * 8 + column
+        seat_id = calculate_seat_id(seat)
 
         if seat_id > max_seat:
             max_seat = seat_id
@@ -36,9 +33,32 @@ def find_max_seat(seats):
     return max_seat
 
 
+def calculate_seat_id(seat):
+    column = find_pos(seat['column_definition'])
+    row = find_pos(seat['row_definition'])
+    seat_id = row * 8 + column
+    return seat_id
+
+
+def find_missing_seat(seats):
+    seat_ids = list()
+    for seat in seats:
+        seat_ids.append(calculate_seat_id(seat))
+
+    seat_ids.sort()
+    missing_id = -1
+
+    while len(seat_ids) != 0:
+        curr_id = seat_ids.pop(0)
+        if curr_id + 1 != seat_ids.pop(0):
+            missing_id = curr_id + 1
+            seat_ids = list()
+
+    return missing_id
+
 if __name__ == '__main__':
     seats = read_input('input_day5.txt')
-    result = find_max_seat(seats)
+    result = find_missing_seat(seats)
     print(f'The result is {result}')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
